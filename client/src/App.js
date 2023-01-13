4f5e40b35a25cc805669a26ffe3e68339bcbba1a
d3vkk <donaldkk22@yahoo.com>
2023-01-13 10:42:24 +0300

    feat: Client interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./normalize.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <aside className="sidemenu">
+          <div className="sidemenu-button">
+            <span>+</span> New Chat
+          </div>
+        </aside>
+        <section className="chatbox">
+          <div className="chat-log">
+            <div className="chat-message">
+              <div className="chat-message-center">
+                <div className="avatar">
+                  <img src="/avatar.svg" alt="" />
+                </div>
+                <div className="message">Hi</div>
+              </div>
+            </div>
+            <div className="chat-message chatgpt">
+              <div className="chat-message-center">
+                <div className="avatar">
+                  <img src="/chatgpt-logo.jpg" alt="" />
+                </div>
+                <div className="message">ChatGPT</div>
+              </div>
+            </div>
+          </div>
+          <div className="chat-input-holder">
+            <textarea className="chat-input-textarea" rows="1"></textarea>
+          </div>
+        </section>
       </div>
     );
   }
